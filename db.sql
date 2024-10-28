701ac3058dc2d7e1e1a43e714311e6709bf7a3ff
Simea <119845281+smjae@users.noreply.github.com>
2024-10-28 15:36:23 +0100

    yay

--- db.sql
+++ db.sql
@@ -1,15 +1,17 @@
-    CREATE TABLE
-    IF NOT EXISTS Wettervorhersage (
-        unixtime DATETIME NOT NULL,
-        temperature DECIMAL(5,2) NOT NULL,
-        tagesniederschlag_sum DECIMAL(6,2) NOT NULL,
-        tagesniederschlag_max DECIMAL(6,2) NOT NULL
+CREATE TABLE IF NOT EXISTS
+    `Wettervorhersage`  (
+        `unixtime` int (11) NOT NULL,
+        `temperature` decimal(5, 2) NOT NULL,
+        `tagesniederschlag_sum` decimal(6, 2) NOT NULL,
+        `tagesniederschlag_max` decimal(6, 2) NOT NULL,
+        `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
     );
 
-
-    CREATE TABLE IF NOT EXISTS Anfragen (
-        unixtime DATETIME NOT NULL,
-        temperature DECIMAL(5,2) NOT NULL,
-        tagesniederschlag_sum DECIMAL(6,2) NOT NULL,
-        tagesniederschlag_max DECIMAL(6,2) NOT NULL
+CREATE TABLE IF NOT EXISTS
+    `Anfragen`  (
+        `unixtime` int (11) NOT NULL,
+        `temperature` decimal(5, 2) NOT NULL,
+        `tagesniederschlag_sum` decimal(6, 2) NOT NULL,
+        `tagesniederschlag_max` decimal(6, 2) NOT NULL,
+        `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
     );
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-console.log("Hello World!");
 // URL is the URL of the server
 // THIS FILE IS ONLY FOR DEVELOPMENT AND TESTING PURPOSES
 const heading = document.createElement("h1");
-heading.innerHTML = "Hello World!";
+heading.innerHTML = "Raincheck";
+const subheading = document.createElement("h2");
+subheading.innerHTML = "Wie wird das Wetter heute in Chur?";
+
 const button = document.createElement("button");
 button.textContent = "Fetch Data";
 button.addEventListener("click", async () => {
@@ -11,8 +13,26 @@ button.addEventListener("click", async () => {
 let response = document.createElement("p");
 response.addClassName = "response";
 
+document.body.appendChild(heading);
+document.body.appendChild(subheading);
 document.body.appendChild(button);
 document.body.appendChild(response);
+
+const tables = document.querySelectorAll("table");
+
+tables.forEach((table, index) => {
+  const rows = Array.from(table.querySelectorAll("tr"));
+  const data = rows.map(row => {
+    const cells = Array.from(row.querySelectorAll("td"));
+    return cells.map(cell => cell.textContent);
+  });
+
+  const div = document.createElement("div");
+div.classList.add(index);
+
+  document.body.appendChild(div);
+    }
+);
 
 async function fetchData() {
   try {
@@ -20,14 +40,6 @@ async function fetchData() {
     const data = await response.json();
     console.log(data);
     processData(data);
-    //save request to table "Anfragen"
-    const response2 = await fetch("https://raincheck.ch/post.php", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
   } catch (error) {
     console.error(error);
   }
@@ -38,4 +50,12 @@ async function processData(data) {
   response.innerHTML = `Current Temperature: ${data.data.currentTemperature[0]}°C, 
     Precipitation Probability: ${data.data.daily_precipitation_probability_max[0]}%, 
     Measured at: ${data.data.currentTime[0]}`;
+    await fetch("https://raincheck.ch/post.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log("Data sent to server", data);
 }
