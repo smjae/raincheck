937d50b1c91d82e4dc5ca890e7af8f80e13f87f2
Simea <119845281+smjae@users.noreply.github.com>
2024-11-11 15:11:55 +0100

    unixtime to datum

--- etl.php
+++ etl.php
@@ -30,7 +30,7 @@ $daily_snowfall_sum = isset($data['daily']['snowfall_sum'][0]) ? $data['daily'][
 $daily_wind_speed_max = isset($data['daily']['wind_speed_10m_max'][0]) ? $data['daily']['wind_speed_10m_max'][0] : NULL;
 
 $weather_data[] = [
-    'unixtime' => $dailyTime,
+    'datum' => $dailyTime,
     'temperatur' => $dailyTemperature,
     'tagesniederschlag_sum' => $daily_precipitation_sum,
     'schneefall_sum' => $daily_snowfall_sum,
@@ -46,15 +46,15 @@ try {
     $pdo = new PDO($dsn, $db_user, $db_pass, $options);
 
     //insert data into table
-    // get data from table, check if unixtime is already in the table, if not insert data
-    $sql = "SELECT * FROM Wettervorhersage ORDER BY unixtime DESC LIMIT 1";
+    // get data from table, check if datum is already in the table, if not insert data
+    $sql = "SELECT * FROM Wettervorhersage ORDER BY datum DESC LIMIT 1";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $last_weather_data = $stmt->fetch();
 
     if (
-        !isset($last_weather_data['unixtime']) ||
-        $last_weather_data['unixtime'] != $weather_data[0]['unixtime'] ||
+        !isset($last_weather_data['datum']) ||
+        $last_weather_data['datum'] != $weather_data[0]['datum'] ||
         $last_weather_data['temperatur'] != $weather_data[0]['temperatur'] ||
         $last_weather_data['tagesniederschlag_sum'] != $weather_data[0]['tagesniederschlag_sum'] ||
         $last_weather_data['schneefall_sum'] != $weather_data[0]['schneefall_sum'] ||
@@ -64,7 +64,7 @@ try {
         echo "<br>";
     
         // SQL-Query mit Platzhaltern für das Einfügen von Daten
-        $sql = "INSERT INTO Wettervorhersage (unixtime, temperatur, tagesniederschlag_sum, schneefall_sum, windgeschwindigkeit_max) VALUES (?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO Wettervorhersage (datum, temperatur, tagesniederschlag_sum, schneefall_sum, windgeschwindigkeit_max) VALUES (?, ?, ?, ?, ?)";
 
 
         // Bereitet die SQL-Anweisung vor
@@ -73,7 +73,7 @@ try {
         // Fügt jedes Element im Array in die Datenbank ein
         foreach ($weather_data as $item) {
             $stmt->execute([
-                $item['unixtime'],
+                $item['datum'],
                 $item['temperatur'],
                 $item['tagesniederschlag_sum'],
                 $item['schneefall_sum'],
