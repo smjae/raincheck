7ce4322f5781ca57150b57e6673f3cd1995436de
Simea <119845281+smjae@users.noreply.github.com>
2024-11-12 09:51:25 +0100

    bruh

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ subheading.innerHTML = "Wie wird das Wetter heute in Chur?";
 document.addEventListener("DOMContentLoaded", async () => {
   const data = await fetchData();
   if (Array.isArray(data)) {
-    processData(data);
     displayAverages(data);
   } else {
     console.error("Fetched data is not an array:", data);
@@ -39,67 +38,6 @@ async function fetchData() {
   } catch (error) {
     console.error(error);
   }
-}
-
-// Process the retrieved data
-function processData(data) {
-  // show data in charts: https://www.chartjs.org/docs/latest/
-  // chart 1: rain sum over time with bar chart, temperature with line chart
-  const ctx1 = document.getElementById('chart1').getContext('2d');
-  new Chart(ctx1, {
-    type: 'bar',
-    data: {
-      labels: data.map(entry => entry.datum),
-      datasets: [
-        {
-          label: 'Rain Sum',
-          data: data.map(entry => entry.tagesniederschlag_sum),
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1
-        },
-        {
-          label: 'Temperature',
-          data: data.map(entry => entry.temperatur),
-          type: 'line',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
-
-  // chart 2: show wind speed over time with bar chart
-  const ctx2 = document.getElementById('chart2').getContext('2d');
-  new Chart(ctx2, {
-    type: 'bar',
-    data: {
-      labels: data.map(entry => entry.datum),
-      datasets: [
-        {
-          label: 'Wind Speed',
-          data: data.map(entry => entry.windgeschwindigkeit_max),
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
 }
 
 // Calculate and display averages
